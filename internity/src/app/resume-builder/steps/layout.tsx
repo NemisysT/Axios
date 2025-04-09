78551c76805e762e7c5412197e3399f960edaa2c
NemisysT <mervin10122004@gmail.com>
2025-04-09 18:57:12 +0530

    Minor Resume Builder Updates

--- internity/src/app/resume-builder/steps/layout.tsx
+++ internity/src/app/resume-builder/steps/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import type React from "react"
-
 import { ResumeFormProvider } from "../components/resume-form-context"
 
 export default function ResumeBuilderLayout({
@@ -11,10 +10,9 @@ export default function ResumeBuilderLayout({
 }) {
   return (
     <ResumeFormProvider>
-      <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-gradient-to-br from-purple-50 to-blue-50">
-        <div className="w-full max-w-4xl">{children}</div>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-purple-50 to-blue-50">
+        <div className="w-full">{children}</div>
       </div>
     </ResumeFormProvider>
   )
 }
-
